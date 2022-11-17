02b44479af8a4524c5ab42c08976060d6a06dbcb
Dev-MukeshKumar <kpmukesh2001@gmail.com>
2022-11-17 14:54:56 +0530

    completed exercise 3

--- src/main/scala/com/credits/Credits.scala
+++ src/main/scala/com/credits/Credits.scala
@@ -10,39 +10,41 @@ import org.apache.spark.rdd.RDD
 import java.util.Properties
 import scala.io.Source
 
-object Credits extends Serializable{
+object Credits extends Serializable {
 
-  @transient lazy val logger: Logger =Logger.getLogger(getClass.getName)
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
 
-  def main(args: Array[String]):Unit = {
+  def main(args: Array[String]): Unit = {
 
     logger.info("Starting credits app.")
 
     val spark = SparkSession.builder().config(getSparkConf()).getOrCreate()
 
-    //a RDD of case class
-    val creditsRddOfCaseClass = getCreditsRddCaseClassSchema(spark)
-    logger.info("RDD of case class Schema:")
-    creditsRddOfCaseClass.foreach(logger.info(_))
-    logger.info("----------------------------------------------------------------")
-
-    //a RDD of case class
-    val creditsRddOfClass = getCreditsRddClassSchema(spark)
-    logger.info("RDD of class Schema:")
-    creditsRddOfClass.foreach(data => logger.info(display(data)))
-    logger.info("----------------------------------------------------------------")
-
-    //a DataFrame
+    //DF to DS
     val creditsDf = getCreditsDf(spark)
-    logger.info("DF of credits showed in console:")
-    creditsDf.filter("creditScore > 130").show()
-    logger.info("----------------------------------------------------------------")
+    import spark.implicits._
+    logger.info("Converting DF to DS")
+    val dfToDs = creditsDf.as[RecordCaseClassSchema]
+    dfToDs.show()
+    logger.info("DF to DS converted")
 
-    //a DataSet of case class
-    val creditsDs = getCreditsDs(spark)
-    logger.info("Ds of credits showed in console:")
-    creditsDs.filter(row => row.creditScore>130).show()
-    logger.info("----------------------------------------------------------------")
+    //DS to RDD
+    logger.info("Converting DS to RDD")
+    val dsToRdd = dfToDs.rdd
+    dsToRdd.foreach(logger.info(_))
+    logger.info("DS to RDD converted")
+
+    //RDD to DS
+    logger.info("Converting RDD to DS")
+    val rddToDs = dsToRdd.toDS()
+    rddToDs.show()
+    logger.info("Converted RDD to DS")
+
+    //RDD to DF
+    logger.info("Converting RDD to DF")
+    val rddToDf = dsToRdd.toDF()
+    rddToDf.show()
+    logger.info("Converted RDD to DF")
 
     spark.stop()
     logger.info("Shutting down credits app.")
@@ -52,14 +54,12 @@ object Credits extends Serializable{
     val sparkConf = new SparkConf()
     val properties = new Properties
     properties.load(Source.fromFile("spark.conf").reader())
-    properties.forEach((k,v) => sparkConf.set(k.toString,v.toString))
+    properties.forEach((k, v) => sparkConf.set(k.toString, v.toString))
     sparkConf
   }
 
   //data parallelize and readers
-  def getCreditsRddCaseClassSchema(spark: SparkSession): RDD[RecordCaseClassSchema] = spark.sparkContext.parallelize(getData(10)).map(row => RecordCaseClassSchema(row._1,row._2,row._3,row._4,row._5,row._6))
-
-  def getCreditsRddClassSchema(spark: SparkSession): RDD[RecordClassSchema] = spark.sparkContext.parallelize(getData(10)).map(row => new RecordClassSchema(row._1,row._2,row._3,row._4,row._5,row._6))
+  def getCreditsRdd(spark: SparkSession): RDD[RecordCaseClassSchema] = spark.sparkContext.parallelize(getData(10)).map(row => RecordCaseClassSchema(row._1, row._2, row._3, row._4, row._5, row._6))
 
   def getCreditsDf(session: SparkSession): Dataset[Row] = session.sqlContext.createDataFrame(getData(10)).toDF("stateCode", "bankId", "areaName", "accountId", "creditScore", "hasCreditCard")
 
@@ -67,7 +67,4 @@ object Credits extends Serializable{
     import session.implicits._
     session.sqlContext.createDataFrame(getData(10)).toDF("stateCode", "bankId", "areaName", "accountId", "creditScore", "hasCreditCard").as[RecordCaseClassSchema]
   }
-
-  //utility functions
-  def display(record: RecordClassSchema) = s"RecordClassSchema(${record.stateCode},${record.bankId},${record.areaName},${record.accountId},${record.creditScore},${record.hasCreditCard})"
 }
